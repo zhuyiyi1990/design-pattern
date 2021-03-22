94614b919a8068a4089e74aa9adc22fffd2f7a58
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-03-22 13:51:58 +0800

    添加外观模式

---
+++ src/main/java/com/github/zhuyiyi1990/designpattern/facadepattern/Rectangle.java
@@ -0,0 +1,10 @@
+package com.github.zhuyiyi1990.designpattern.facadepattern;
+
+public class Rectangle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Rectangle::draw()");
+    }
+
+}
---
+++ src/main/java/com/github/zhuyiyi1990/designpattern/facadepattern/Square.java
@@ -0,0 +1,10 @@
+package com.github.zhuyiyi1990.designpattern.facadepattern;
+
+public class Square implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Square::draw()");
+    }
+
+}
