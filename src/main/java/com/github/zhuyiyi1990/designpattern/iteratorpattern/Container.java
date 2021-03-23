924fed29c25537c9d31d724706abaeff3b465677
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-03-23 11:16:29 +0800

    添加迭代器模式

---
+++ src/main/java/com/github/zhuyiyi1990/designpattern/iteratorpattern/Container.java
@@ -0,0 +1,7 @@
+package com.github.zhuyiyi1990.designpattern.iteratorpattern;
+
+public interface Container {
+
+    public abstract Iterator getIterator();
+
+}
---
+++ src/main/java/com/github/zhuyiyi1990/designpattern/iteratorpattern/Iterator.java
@@ -0,0 +1,9 @@
+package com.github.zhuyiyi1990.designpattern.iteratorpattern;
+
+public interface Iterator {
+
+    public abstract boolean hasNext();
+
+    public abstract Object next();
+
+}
