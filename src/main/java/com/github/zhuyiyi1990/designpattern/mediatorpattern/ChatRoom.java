6e62ccfe930ec212758ee5d15f5ae5808071b992
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-03-23 15:33:40 +0800

    添加中介者模式

---
+++ src/main/java/com/github/zhuyiyi1990/designpattern/mediatorpattern/ChatRoom.java
@@ -0,0 +1,11 @@
+package com.github.zhuyiyi1990.designpattern.mediatorpattern;
+
+import java.util.Date;
+
+public class ChatRoom {
+
+    public static void showMessage(User user, String message) {
+        System.out.println(new Date().toString() + " [" + user.getName() + "] : " + message);
+    }
+
+}
