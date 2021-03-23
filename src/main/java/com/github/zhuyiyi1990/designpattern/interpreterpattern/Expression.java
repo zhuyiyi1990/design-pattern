6f15db017f35e3f3314cfc10ec6145941ccd5250
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-03-23 10:57:21 +0800

    添加解释器模式

---
+++ src/main/java/com/github/zhuyiyi1990/designpattern/interpreterpattern/Expression.java
@@ -0,0 +1,7 @@
+package com.github.zhuyiyi1990.designpattern.interpreterpattern;
+
+public interface Expression {
+
+    public abstract boolean interpret(String context);
+
+}
---
+++ src/main/java/com/github/zhuyiyi1990/designpattern/interpreterpattern/TerminalExpression.java
@@ -0,0 +1,19 @@
+package com.github.zhuyiyi1990.designpattern.interpreterpattern;
+
+public class TerminalExpression implements Expression {
+
+    private String data;
+
+    public TerminalExpression(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        if (context.contains(data)) {
+            return true;
+        }
+        return false;
+    }
+
+}
