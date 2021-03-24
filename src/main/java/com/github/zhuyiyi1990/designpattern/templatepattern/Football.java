d199269bc3ba6b18fff165e5afc9ba945f10a550
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-03-24 17:43:45 +0800

    添加模板模式

---
+++ src/main/java/com/github/zhuyiyi1990/designpattern/templatepattern/Football.java
@@ -0,0 +1,20 @@
+package com.github.zhuyiyi1990.designpattern.templatepattern;
+
+public class Football extends Game {
+
+    @Override
+    void initialize() {
+        System.out.println("Football Game Initialized! Start playing.");
+    }
+
+    @Override
+    void startPlay() {
+        System.out.println("Football Game Started. Enjoy the game!");
+    }
+
+    @Override
+    void endPlay() {
+        System.out.println("Football Game Finished!");
+    }
+
+}
