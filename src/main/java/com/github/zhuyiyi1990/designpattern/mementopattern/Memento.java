21e4fbaf828671bc5f904227b5cdf29eb1488bba
zhu_yiyi <zhuyiyi1990@outlook.com>
2021-03-24 15:15:55 +0800

    添加备忘录模式

---
+++ src/main/java/com/github/zhuyiyi1990/designpattern/mementopattern/Memento.java
@@ -0,0 +1,15 @@
+package com.github.zhuyiyi1990.designpattern.mementopattern;
+
+public class Memento {
+
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+}
---
+++ src/main/java/com/github/zhuyiyi1990/designpattern/mementopattern/Originator.java
@@ -0,0 +1,23 @@
+package com.github.zhuyiyi1990.designpattern.mementopattern;
+
+public class Originator {
+
+    private String state;
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public Memento saveStateToMemento() {
+        return new Memento(state);
+    }
+
+    public void getStateFromMemento(Memento Memento) {
+        state = Memento.getState();
+    }
+
+}
